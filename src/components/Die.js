0075b3d0be43579b46ee0647504cea1f78ee61bb
jonathancklee1 <jonathancklee@gmail.com>
2022-05-16 19:03:52 +1000

    Added dotted dice faces

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,6 +2,103 @@ import React from "react";
 
 export default function Die(props) {
   const selected = props.isHeld ? "die isHeld" : "die";
+  let face;
+  function setDiceFace() {
+    // Renders dots on dice
+    switch (props.value) {
+      case 1:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1 none"></div>
+            <div className="dot a2 none"></div>
+            <div className="dot a3 none"></div>
+            <div className="dot a4"></div>
+            <div className="dot a5 none"></div>
+            <div className="dot a6 none"></div>
+            <div className="dot a7 none"></div>
+          </div>
+        );
+        break;
+      case 2:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1 "></div>
+            <div className="dot a2 none"></div>
+            <div className="dot a3 none"></div>
+            <div className="dot a4 none"></div>
+            <div className="dot a5 none"></div>
+            <div className="dot a6 none"></div>
+            <div className="dot a7 "></div>
+          </div>
+        );
+        break;
+      case 3:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1 "></div>
+            <div className="dot a2 none"></div>
+            <div className="dot a3 none"></div>
+            <div className="dot a4"></div>
+            <div className="dot a5 none"></div>
+            <div className="dot a6 none"></div>
+            <div className="dot a7 "></div>
+          </div>
+        );
+        break;
+      case 4:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1"></div>
+            <div className="dot a2 none"></div>
+            <div className="dot a3"></div>
+            <div className="dot a4 none"></div>
+            <div className="dot a5"></div>
+            <div className="dot a6 none"></div>
+            <div className="dot a7 "></div>
+          </div>
+        );
+        break;
+      case 5:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1 "></div>
+            <div className="dot a2 none"></div>
+            <div className="dot a3 "></div>
+            <div className="dot a4 "></div>
+            <div className="dot a5 "></div>
+            <div className="dot a6 none"></div>
+            <div className="dot a7 "></div>
+          </div>
+        );
+        break;
+      case 6:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1 "></div>
+            <div className="dot a2 "></div>
+            <div className="dot a3 "></div>
+            <div className="dot a4 none"></div>
+            <div className="dot a5 "></div>
+            <div className="dot a6 "></div>
+            <div className="dot a7 "></div>
+          </div>
+        );
+        break;
+      default:
+        face = (
+          <div className="dots-wrapper">
+            <div className="dot a1 "></div>
+            <div className="dot a2 "></div>
+            <div className="dot a3 "></div>
+            <div className="dot a4 none"></div>
+            <div className="dot a5 "></div>
+            <div className="dot a6 "></div>
+            <div className="dot a7 "></div>
+          </div>
+        );
+    }
+    return face;
+  }
   return (
     <div
       className={selected}
@@ -9,7 +106,7 @@ export default function Die(props) {
         props.holdDice(props.id);
       }}
     >
-      <p className="value">{props.value}</p>
+      {setDiceFace()}
     </div>
   );
 }
